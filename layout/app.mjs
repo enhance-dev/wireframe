ce5ec848f8e8c0a0419978396b32034f417091e3
kj <kj@begin.com>
2023-07-13 17:06:29 -0700

    Moves to mjs

--- layout/app.mjs
+++ layout/app.mjs
@@ -3,9 +3,16 @@ export default function AppLayout({ html }) {
     <style>
       :host {
         --default-header-height: 4rem;
-        --default-sidebar-transition: transform 0.25s cubic-bezier(.86,0,.07,1);
+        --default-sidebar-transition: translate 0.25s cubic-bezier(.86,0,.07,1);
         --default-sidebar-min-width: 15rem;
         height: 100dvh;
+        display: grid;
+        grid-template-areas:
+          'header'
+          'sidebar'
+          'main'
+          'footer';
+        grid-template-rows: min-content min-content 1fr min-content;
         overflow: hidden;
       }
 
@@ -23,7 +30,7 @@ export default function AppLayout({ html }) {
         right: 0;
         bottom: 0;
         left: 0;
-        transform: translateX(-100%);
+        translate: -100% 0;
         transition: var(--layout-sidebar-transition, var(--default-sidebar-transition));
       }
 
@@ -32,13 +39,15 @@ export default function AppLayout({ html }) {
       }
 
       [slot='header']:has(input[type='checkbox']:checked) + [slot='sidebar'] {
-        transform: translateX(0);
+        translate: 0 0;
       }
 
       @media only screen and (min-width:48em) {
 
         :host {
           display: grid;
+          grid-template:
+            'header header' min-content
             'sidebar main' 1fr
             'sidebar footer' min-content
             / var(--sidebar-min-width, var(--default-sidebar-min-width)) 1fr;
@@ -51,7 +60,7 @@ export default function AppLayout({ html }) {
 
         ::slotted([slot='sidebar']) {
           position: static;
-          transform: initial;
+          translate: initial;
           transition: initial;
         }
       }
